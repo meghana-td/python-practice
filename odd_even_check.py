07a57f91796d7c2a473dc15868d456f28a0988be
Meghana T D <meghanatd276@gmail.com>
2026-06-09 23:48:28 +0530

    Added odd or even checker
    A python program to check user given number is odd or even by using conditional statements.

---
+++ odd_even_check.py
@@ -0,0 +1,7 @@
+print("---Even or Odd checker---")
+n=int(input("Enter a number to check: "))
+# To check the number is odd or even
+if n%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
